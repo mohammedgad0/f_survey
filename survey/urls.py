93e179f754681e6539346d1e260486800127cd81
mag <mohammedgad09@gmail.com>
2018-07-25 15:37:21 +0300

    template

---
+++ survey/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.urls import path, re_path
+from survey.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include, url
+
+app_name = 'survey'
+
+urlpatterns = [
+    # Home
+    path('', home, name='home'),
+
+]
